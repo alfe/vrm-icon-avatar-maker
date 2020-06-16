bdf5d680976d5f23a8d96f53651a3e27c21a69f7
alfe <alfe10251@gmail.com>
2020-06-16 21:22:00 +0900

    color change maker

--- app.js
+++ app.js
@@ -7,15 +7,11 @@ var fs = require('fs');
 var multer = require('multer');
 
 const { ConvertGLBtoGltf, ConvertGltfToGLB, ConvertToGLB} = require('gltf-import-export');
-const GltfFile = './uploads/icon_avatar.vrm.gltf';
-const GltfFileRobo = './uploads/IconAvatarRobo.gltf';
+const GltfFile = './uploads/5988710431914478867.gltf';
 let gltf;
 let gltfRobo;
 fs.readFile(GltfFile, 'utf8', (err, text) => {
-	gltf = text;
-});
-fs.readFile(GltfFileRobo, 'utf8', (err, text) => {
-	gltfRobo = text;
+  gltf = text;
 });
 
 var indexRouter = require('./routes/index');
@@ -36,40 +32,47 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 var storage = multer.diskStorage({
-          destination: function (req, file, cb) {
-                      cb(null, './uploads')
-                    },
-          filename: function (req, file, cb) {
-                      cb(null, '[' + req.body.username + ']' + file.originalname)
-                    }
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, '[' + req.body.username + ']' + file.originalname)
+  }
 });
 var upload = multer({ storage: storage });
 
-app.post('/', upload.single('uploadedfile'), function (req, res) {
+app.post('/vrm/', upload.single('uploadedfile'), (req, res) => {
   let returnGltf, glbFileName = '';
-
+  
   // for white human
   returnGltf = JSON.parse(gltf);
-  returnGltf.images = [{
-    name: "icon",
-    uri: './' + '[' + req.body.username + ']' + req.file.originalname,
-  }];
+  const hex2rgb = ( hex = "" ) => {
+    if ( hex.slice(0, 1) == "#" ) hex = hex.slice(1) ;
+    if ( hex.length == 3 ) hex = hex.slice(0,1) + hex.slice(0,1) + hex.slice(1,2) + hex.slice(1,2) + hex.slice(2,3) + hex.slice(2,3) ;
+  
+    return [ hex.slice( 0, 2 ), hex.slice( 2, 4 ), hex.slice( 4, 6 ) ].map(( str ) => {
+      return parseInt( str, 16 ) / 255;
+    } ) ;
+  }
+  console.log(hex2rgb(req.body.color));
+  returnGltf.materials[1].pbrMetallicRoughness.baseColorFactor = [...hex2rgb(req.body.color), 1];
+  console.log(returnGltf.materials[1].pbrMetallicRoughness.baseColorFactor);
   glbFileName = GltfFile;
-
+  
   returnGltf.extensions.VRM.meta.author = req.body.username;
   returnGltf.extensions.VRM.meta.contactInformation = "@" + req.body.username;
   returnGltf.extensions.VRM.meta.title = "icon_avatar_" + req.body.username;
-
+  
   const outputGlb = './vrm/' + req.body.username + '.vrm';
-    ConvertToGLB(returnGltf, glbFileName, outputGlb);
-    res.redirect(301, '/vrms/' + req.body.username + '.vrm');
+  ConvertToGLB(returnGltf, glbFileName, outputGlb);
+  res.redirect(301, '/vrms/' + req.body.username + '.vrm');
 });
 
 app.get('/vrms/:file', function(req, res){
-    fs.readFile('./vrm/' + req.params.file,
-        function(err, data) {
-        res.send(data, 200);
-    });
+  fs.readFile('./vrm/' + req.params.file,
+  function(err, data) {
+    res.send(data, 200);
+  });
 });
 
 app.use('/users', usersRouter);
@@ -84,7 +87,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  
   // render the error page
   res.status(err.status || 500);
   res.render('error');
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'VRM ICON AVATAR MAKER' });
+  res.render('index', { title: 'VRM AVATAR MAKER' });
 });
 
 module.exports = router;
